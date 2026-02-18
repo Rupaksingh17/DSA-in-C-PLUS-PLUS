aa46b8f9687f96dfca283e3781447f41e51cf25f
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2026-02-19 00:31:27 +0530

    min

---
+++ MINelement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+ int n;
+ cin>>n;
+ int arr[n];
+ int ans=INT_MAX;
+ for (int i=0;i<n;i++){
+ cin>>arr[i];
+ if(arr[i]<ans)
+ {
+    ans=arr[i];
+ }
+ }
+ cout<<ans;
+}
